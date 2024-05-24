aee9fde393962928a44945334d628b6efa8b7d11
jcasben <jcasben02@gmail.com>
2024-05-24 23:16:40 +0200

    [NEW]: added new project IMCCalculator

---
+++ IMCCalculator/app/src/main/java/com/jcasben/imccalculator/ImcCalculatorActivity.kt
@@ -0,0 +1,63 @@
+package com.jcasben.imccalculator
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
+
+class ImcCalculatorActivity : AppCompatActivity() {
+
+    private var isMaleSelected: Boolean = true
+    private var isFemaleSelected: Boolean = false
+
+    private lateinit var viewMale: CardView
+    private lateinit var viewFemale: CardView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initComponents()
+        initListeners()
+        initUI()
+    }
+
+    private fun initComponents() {
+        viewMale = findViewById(R.id.viewMale)
+        viewFemale = findViewById(R.id.viewFemale)
+    }
+
+    private fun initListeners() {
+        viewMale.setOnClickListener {
+            changeGender()
+            setGenderColor()
+        }
+        viewFemale.setOnClickListener {
+            changeGender()
+            setGenderColor()
+        }
+    }
+
+    private fun changeGender() {
+        isMaleSelected = !isMaleSelected
+        isFemaleSelected = !isFemaleSelected
+    }
+
+    private fun setGenderColor() {
+        viewMale.setCardBackgroundColor(getBackgroundColor(isMaleSelected))
+        viewFemale.setCardBackgroundColor(getBackgroundColor(isFemaleSelected))
+    }
+
+    private fun getBackgroundColor(isComponentSelected: Boolean): Int {
+        val color = if (isComponentSelected) {
+            R.color.background_component_selected
+        } else {
+            R.color.background_component
+        }
+
+        return ContextCompat.getColor(this, color)
+    }
+
+    private fun initUI() {
+        setGenderColor()
+    }
+}
