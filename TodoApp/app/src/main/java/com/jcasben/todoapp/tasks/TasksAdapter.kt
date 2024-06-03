c8ab1eea36c60f77006f1bfbc53f2a8494d707ad
jcasben <jcasben02@gmail.com>
2024-06-03 22:30:07 +0200

    [UPDATE]: implemented Recycler view components

--- TodoApp/app/src/main/java/com/jcasben/todoapp/tasks/TasksAdapter.kt
+++ TodoApp/app/src/main/java/com/jcasben/todoapp/tasks/TasksAdapter.kt
@@ -1,17 +1,21 @@
 package com.jcasben.todoapp.tasks
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.jcasben.todoapp.R
 
 class TasksAdapter(private val tasks: List<Task>) : RecyclerView.Adapter<TasksViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TasksViewHolder {
-        TODO("Not yet implemented")
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_task, parent, false)
+        return TasksViewHolder(view)
     }
 
     override fun getItemCount(): Int = tasks.size
 
     override fun onBindViewHolder(holder: TasksViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.render(tasks[position])
     }
 }
--- TodoApp/app/src/main/java/com/jcasben/todoapp/tasks/TasksViewHolder.kt
+++ TodoApp/app/src/main/java/com/jcasben/todoapp/tasks/TasksViewHolder.kt
@@ -1,11 +1,17 @@
 package com.jcasben.todoapp.tasks
 
 import android.view.View
+import android.widget.CheckBox
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.jcasben.todoapp.R
 
 class TasksViewHolder (view: View) : RecyclerView.ViewHolder(view) {
 
-    fun render(task: Task) {
+    private val tvTask: TextView = view.findViewById(R.id.tvTask)
+    private val cbTask: CheckBox = view.findViewById(R.id.cbTask)
 
+    fun render(task: Task) {
+        tvTask.text = task.name
     }
 }
