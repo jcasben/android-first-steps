12e3da3b03696f76272c0e9f89bc8d5497c3ea9a
jcasben <jcasben02@gmail.com>
2024-06-19 22:02:24 +0200

    [UPDATE]: added original, english and image to anime details layout

--- AnimesApp/app/src/main/java/com/jcasben/heroesapp/animes/details/DetailsAnimeActivity.kt
+++ AnimesApp/app/src/main/java/com/jcasben/heroesapp/animes/details/DetailsAnimeActivity.kt
@@ -1,11 +1,13 @@
 package com.jcasben.heroesapp.animes.details
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.jcasben.heroesapp.AnimeDetailResponse
 import com.jcasben.heroesapp.ApiService
 import com.jcasben.heroesapp.R
 import com.jcasben.heroesapp.databinding.ActivityDetailsAnimeBinding
+import com.squareup.picasso.Picasso
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -31,14 +33,18 @@ class DetailsAnimeActivity : AppCompatActivity() {
     private fun getAnimeInfo(id: String) {
         CoroutineScope(Dispatchers.IO).launch {
             val animeDetail = getRetrofit().create(ApiService::class.java).getAnimeDetails(id)
+            Log.i("test", animeDetail.raw().toString())
             if (animeDetail.body() != null) {
+                Log.i("test", animeDetail.body().toString())
                 runOnUiThread { createScreen(animeDetail.body()!!) }
             }
         }
     }
 
     private fun createScreen(anime: AnimeDetailResponse) {
-
+        binding.tvOriginalName.text = anime.originalName
+        binding.tvEnglishName.text = anime.engName
+        Picasso.get().load(anime.picUrl).into(binding.ivAnimeImage)
     }
 
     private fun getRetrofit(): Retrofit {
