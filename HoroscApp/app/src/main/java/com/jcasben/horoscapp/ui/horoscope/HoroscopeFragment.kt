a9bfddd7e56f8686f1363263fddbc77e3f7ba5ed
jcasben <jcasben02@gmail.com>
2024-06-24 13:47:30 +0200

    [UPDATE]: connected fragment with horoscope viewmodel

--- HoroscApp/app/src/main/java/com/jcasben/horoscapp/ui/horoscope/HoroscopeFragment.kt
+++ HoroscApp/app/src/main/java/com/jcasben/horoscapp/ui/horoscope/HoroscopeFragment.kt
@@ -1,18 +1,57 @@
 package com.jcasben.horoscapp.ui.horoscope
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.jcasben.horoscapp.databinding.FragmentHoroscopeBinding
+import com.jcasben.horoscapp.ui.horoscope.adapter.HoroscopeAdapter
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HoroscopeFragment : Fragment() {
 
+    private val horoscopeViewModel by viewModels<HoroscopeViewModel>()
+    private lateinit var horoscopeAdapter: HoroscopeAdapter
+
     private var _binding: FragmentHoroscopeBinding? = null
     private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initUI()
+    }
+
+    private fun initUI() {
+        initRecyclerView()
+        initUIState()
+    }
+
+    private fun initRecyclerView() {
+        horoscopeAdapter = HoroscopeAdapter()
+        binding.rvHoroscope.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = horoscopeAdapter
+        }
+    }
+
+    private fun initUIState() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                horoscopeViewModel.horoscope.collect {
+                    horoscopeAdapter.updateList(it)
+                }
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
