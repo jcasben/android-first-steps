9b8b32c0fe18232476cc5c4289435d9ea930105c
jcasben <jcasben02@gmail.com>
2024-06-29 01:20:53 +0200

    [NEW]: created api call

---
+++ HoroscApp/app/src/main/java/com/jcasben/horoscapp/data/network/HoroscopeApiService.kt
@@ -0,0 +1,11 @@
+package com.jcasben.horoscapp.data.network
+
+import com.jcasben.horoscapp.data.network.response.PredictionResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface HoroscopeApiService {
+
+    @GET("/{sign}")
+    suspend fun getHoroscope(@Path("sign") sign: String): PredictionResponse
+}
