09e6c317db6938c68c10a2bde707c5545a3bd883
jcasben <jcasben02@gmail.com>
2024-06-29 01:17:55 +0200

    [UPDATE]: added view model implementation

--- HoroscApp/app/src/main/java/com/jcasben/horoscapp/ui/detail/HoroscopeDetailActivity.kt
+++ HoroscApp/app/src/main/java/com/jcasben/horoscapp/ui/detail/HoroscopeDetailActivity.kt
@@ -1,15 +1,16 @@
 package com.jcasben.horoscapp.ui.detail
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.navArgs
-import com.jcasben.horoscapp.R
 import com.jcasben.horoscapp.databinding.ActivityHoroscopeDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HoroscopeDetailActivity : AppCompatActivity() {
@@ -23,6 +24,40 @@ class HoroscopeDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityHoroscopeDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        args.type
+        horoscopeDetailViewModel.getHoroscope(args.type.name)
+        initUI()
+    }
+
+    private fun initUI() {
+        initUIState()
+    }
+
+    private fun initUIState() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                horoscopeDetailViewModel.state.collect {
+                    when(it) {
+                        is HoroscopeDetailState.Error -> errorState()
+                        HoroscopeDetailState.Loading -> loadingState()
+                        is HoroscopeDetailState.Success -> successState(it)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun successState(state: HoroscopeDetailState.Success) {
+        binding.progressBar.isVisible = false
+        binding.tvTitle.text = state.sign
+        binding.tvBody.text = state.prediction
+    }
+
+    private fun errorState() {
+        binding.progressBar.isVisible = false
+
+    }
+
+    private fun loadingState() {
+        binding.progressBar.isVisible = true
     }
 }
