ec76ee0d1d2c4e99a021b2f600ed28a1cf934a84
jcasben <jcasben02@gmail.com>
2024-06-29 19:27:47 +0200

    [UPDATE]: added image to details activity

--- HoroscApp/app/src/main/java/com/jcasben/horoscapp/ui/detail/HoroscopeDetailActivity.kt
+++ HoroscApp/app/src/main/java/com/jcasben/horoscapp/ui/detail/HoroscopeDetailActivity.kt
@@ -8,7 +8,20 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.navArgs
+import com.jcasben.horoscapp.R
 import com.jcasben.horoscapp.databinding.ActivityHoroscopeDetailBinding
+import com.jcasben.horoscapp.domain.model.HoroscopeModel.Aquarius
+import com.jcasben.horoscapp.domain.model.HoroscopeModel.Aries
+import com.jcasben.horoscapp.domain.model.HoroscopeModel.Cancer
+import com.jcasben.horoscapp.domain.model.HoroscopeModel.Capricorn
+import com.jcasben.horoscapp.domain.model.HoroscopeModel.Gemini
+import com.jcasben.horoscapp.domain.model.HoroscopeModel.Leo
+import com.jcasben.horoscapp.domain.model.HoroscopeModel.Libra
+import com.jcasben.horoscapp.domain.model.HoroscopeModel.Pisces
+import com.jcasben.horoscapp.domain.model.HoroscopeModel.Sagittarius
+import com.jcasben.horoscapp.domain.model.HoroscopeModel.Scorpio
+import com.jcasben.horoscapp.domain.model.HoroscopeModel.Taurus
+import com.jcasben.horoscapp.domain.model.HoroscopeModel.Virgo
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
@@ -24,12 +37,17 @@ class HoroscopeDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityHoroscopeDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        horoscopeDetailViewModel.getHoroscope(args.type.name)
+        horoscopeDetailViewModel.getHoroscope(args.type)
         initUI()
     }
 
     private fun initUI() {
+        initListeners()
         initUIState()
+    }
+
+    private fun initListeners() {
+        binding.ivBack.setOnClickListener { onBackPressedDispatcher.onBackPressed() }
     }
 
     private fun initUIState() {
@@ -50,11 +68,27 @@ class HoroscopeDetailActivity : AppCompatActivity() {
         binding.progressBar.isVisible = false
         binding.tvTitle.text = state.sign
         binding.tvBody.text = state.prediction
+
+        val image = when(state.horoscopeModel) {
+            Aries -> R.drawable.detail_aries
+            Aquarius -> R.drawable.detail_aquarius
+            Cancer -> R.drawable.detail_cancer
+            Capricorn -> R.drawable.detail_capricorn
+            Gemini -> R.drawable.detail_gemini
+            Libra -> R.drawable.detail_libra
+            Leo -> R.drawable.detail_leo
+            Pisces -> R.drawable.detail_pisces
+            Sagittarius -> R.drawable.detail_sagittarius
+            Scorpio -> R.drawable.detail_scorpio
+            Taurus -> R.drawable.detail_taurus
+            Virgo -> R.drawable.detail_virgo
+        }
+
+        binding.ivDetail.setImageResource(image)
     }
 
     private fun errorState() {
         binding.progressBar.isVisible = false
-
     }
 
     private fun loadingState() {
--- HoroscApp/app/src/main/java/com/jcasben/horoscapp/ui/detail/HoroscopeDetailViewModel.kt
+++ HoroscApp/app/src/main/java/com/jcasben/horoscapp/ui/detail/HoroscopeDetailViewModel.kt
@@ -2,6 +2,7 @@ package com.jcasben.horoscapp.ui.detail
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.jcasben.horoscapp.domain.model.HoroscopeModel
 import com.jcasben.horoscapp.domain.usecase.GetPredictionUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
@@ -18,12 +19,15 @@ class HoroscopeDetailViewModel @Inject constructor(private val getPredictionUseC
     private var _state = MutableStateFlow<HoroscopeDetailState>(HoroscopeDetailState.Loading)
     val state: StateFlow<HoroscopeDetailState> = _state
 
-    fun getHoroscope(sign: String) {
+    lateinit var horoscope: HoroscopeModel
+
+    fun getHoroscope(sign: HoroscopeModel) {
+        horoscope = sign
         viewModelScope.launch {
             _state.value = HoroscopeDetailState.Loading
-            val result = withContext(Dispatchers.IO) { getPredictionUseCase(sign) }
+            val result = withContext(Dispatchers.IO) { getPredictionUseCase(sign.name) }
             if (result != null) _state.value =
-                HoroscopeDetailState.Success(result.prediction, result.sign)
+                HoroscopeDetailState.Success(result.prediction, result.sign, horoscope)
             else _state.value = HoroscopeDetailState.Error("An error occurred. Try again later.")
         }
     }
