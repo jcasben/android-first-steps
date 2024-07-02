72d8a5a4de7fd1ef82d76c9a54bac3cc9e8aa59e
jcasben <jcasben02@gmail.com>
2024-07-03 00:30:05 +0200

    [UPDATE]: developed luck fragment

--- HoroscApp/app/src/main/java/com/jcasben/horoscapp/ui/luck/LuckFragment.kt
+++ HoroscApp/app/src/main/java/com/jcasben/horoscapp/ui/luck/LuckFragment.kt
@@ -1,18 +1,69 @@
 package com.jcasben.horoscapp.ui.luck
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.Animation.AnimationListener
+import android.view.animation.AnimationUtils
+import android.view.animation.DecelerateInterpolator
+import androidx.core.animation.doOnEnd
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import com.jcasben.horoscapp.R
 import com.jcasben.horoscapp.databinding.FragmentLuckBinding
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.Random
 
 @AndroidEntryPoint
 class LuckFragment : Fragment() {
 
     private var _binding: FragmentLuckBinding? = null
     private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initUI()
+    }
+
+    private fun initUI() {
+        initListeners()
+    }
+
+    private fun initListeners() {
+        binding.ivRoulette.setOnClickListener { spinRoulette() }
+    }
+
+    private fun spinRoulette() {
+        val random = Random()
+        val degrees = random.nextInt(1440) + 360
+        val animator =
+            ObjectAnimator.ofFloat(binding.ivRoulette, View.ROTATION, 0f, degrees.toFloat())
+        animator.duration = 2000
+        animator.interpolator = DecelerateInterpolator()
+        animator.doOnEnd { slideCard() }
+        animator.start()
+    }
+
+    private fun slideCard() {
+        val slideUpAnimator = AnimationUtils.loadAnimation(requireContext(), R.anim.slide_up)
+        slideUpAnimator.setAnimationListener(object : AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {
+                binding.reverseCard.isVisible = true
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+
+            }
+        })
+        binding.reverseCard.startAnimation(slideUpAnimator)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
