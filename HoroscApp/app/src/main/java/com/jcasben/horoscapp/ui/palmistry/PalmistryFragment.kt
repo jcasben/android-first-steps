240d44b5ef4af9191a2794a12f55c9ed8d46cbe1
jcasben <jcasben02@gmail.com>
2024-07-04 00:52:35 +0200

    [UPDATE]: finished palmistry screen

--- HoroscApp/app/src/main/java/com/jcasben/horoscapp/ui/palmistry/PalmistryFragment.kt
+++ HoroscApp/app/src/main/java/com/jcasben/horoscapp/ui/palmistry/PalmistryFragment.kt
@@ -1,11 +1,16 @@
 package com.jcasben.horoscapp.ui.palmistry
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import androidx.core.content.PermissionChecker
 import androidx.fragment.app.Fragment
 import com.jcasben.horoscapp.databinding.FragmentPalmistryBinding
@@ -25,7 +30,7 @@ class PalmistryFragment : Fragment() {
         ActivityResultContracts.RequestPermission()
     ) { isGranted ->
         if (isGranted) {
-
+            startCamera()
         } else {
             Toast.makeText(
                 requireContext(),
@@ -38,10 +43,30 @@ class PalmistryFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         if (checkCameraPermission()) {
-
+            startCamera()
         } else {
             requestPermissionLauncher.launch(CAMERA_PERMISSION)
         }
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+        cameraProviderFuture.addListener({
+            val cameraProvider = cameraProviderFuture.get()
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+            } catch (e: Exception) {
+                Log.e("exception", "ERROR: ${e.message}")
+            }
+        }, ContextCompat.getMainExecutor(requireContext()))
     }
 
     fun checkCameraPermission(): Boolean {
