f7b7a2bf9573dbe2377faf98fd5d975fbb5b2d29
jcasben <jcasben02@gmail.com>
2024-07-30 18:47:19 +0200

    [UPDATE]: added State Property

--- ComposeCourse/app/src/main/java/com/jcasben/composecourse/MainActivity.kt
+++ ComposeCourse/app/src/main/java/com/jcasben/composecourse/MainActivity.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
@@ -23,10 +22,10 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -53,17 +52,17 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun MyState() {
-    val counter = rememberSaveable { mutableIntStateOf(0) }
+    var counter by rememberSaveable { mutableIntStateOf(0) }
 
     Column(
         Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Button(onClick = { counter.intValue += 1 }) {
+        Button(onClick = { counter += 1 }) {
             Text(text = "Click")
         }
-        Text(text = "I have been clicked ${counter.intValue} times!")
+        Text(text = "I have been clicked $counter times!")
     }
 }
 
