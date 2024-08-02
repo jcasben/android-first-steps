100d8f5aed03f5ed7c4c3529914b8e4a9ddabed8
jcasben <jcasben02@gmail.com>
2024-08-03 00:37:00 +0200

    [UPDATE]: moved Slider

--- JetpackComponentsCatalog/app/src/main/java/com/jcasben/jetpackcomponentscatalog/Slider.kt
+++ JetpackComponentsCatalog/app/src/main/java/com/jcasben/jetpackcomponentscatalog/Slider.kt
@@ -1,4 +1,4 @@
-package com.jcasben.jetpackcomponentscatalog.ui.theme
+package com.jcasben.jetpackcomponentscatalog
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Slider
