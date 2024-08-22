347c6b8c9047e8cfa5101fbe02f5ec7045b2fe8a
jcasben <jcasben02@gmail.com>
2024-08-23 00:24:14 +0200

    [UPDATE]: added retrofit

--- ComposeInstagramLogin/app/build.gradle.kts
+++ ComposeInstagramLogin/app/build.gradle.kts
@@ -61,6 +61,8 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.material.icons.extended)
     implementation("androidx.compose.runtime:runtime-livedata:1.6.8")
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
