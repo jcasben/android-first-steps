915f8876328bf23d2e226344229143b75721828a
jcasben <jcasben02@gmail.com>
2024-08-23 03:17:06 +0200

    [UPDATE]: implemented Hilt

--- ComposeInstagramLogin/app/src/main/java/com/jcasben/composeinstagramlogin/MainActivity.kt
+++ ComposeInstagramLogin/app/src/main/java/com/jcasben/composeinstagramlogin/MainActivity.kt
@@ -3,16 +3,21 @@ package com.jcasben.composeinstagramlogin
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import com.jcasben.composeinstagramlogin.login.ui.LoginScreen
 import com.jcasben.composeinstagramlogin.login.ui.LoginViewModel
 import com.jcasben.composeinstagramlogin.ui.theme.ComposeInstagramLoginTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val loginViewModel: LoginViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeInstagramLoginTheme {
-                LoginScreen(LoginViewModel())
+                LoginScreen(loginViewModel)
             }
         }
     }
