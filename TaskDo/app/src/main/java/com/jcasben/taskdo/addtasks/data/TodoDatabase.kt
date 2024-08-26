9dddf86f341df6d4937fd3644d33ec8d73e3b170
jcasben <jcasben02@gmail.com>
2024-08-26 18:41:03 +0200

    [NEW]: created Room database and Dao

---
+++ TaskDo/app/src/main/java/com/jcasben/taskdo/addtasks/data/TodoDatabase.kt
@@ -0,0 +1,9 @@
+package com.jcasben.taskdo.addtasks.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [TaskEntity::class], version = 1)
+abstract class TodoDatabase : RoomDatabase() {
+    abstract fun taskDao(): TaskDao
+}
