f1f09585d5e8a71d1273211b6fad0b33911444fc
jcasben <jcasben02@gmail.com>
2024-08-26 18:42:35 +0200

    [NEW]: created Entity for Room

---
+++ TaskDo/app/src/main/java/com/jcasben/taskdo/addtasks/data/TaskEntity.kt
@@ -0,0 +1,12 @@
+package com.jcasben.taskdo.addtasks.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class TaskEntity(
+    @PrimaryKey
+    val id: Int,
+    val task: String,
+    var selected: Boolean = false
+)
