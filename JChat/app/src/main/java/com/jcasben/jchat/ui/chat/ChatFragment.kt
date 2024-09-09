69c4bf6ef1122491a67cea6699793480bdde3cec
jcasben <jcasben02@gmail.com>
2024-09-09 18:44:55 +0200

    [FEAT]: designed messages

--- JChat/app/src/main/java/com/jcasben/jchat/ui/chat/ChatFragment.kt
+++ JChat/app/src/main/java/com/jcasben/jchat/ui/chat/ChatFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.jcasben.jchat.R
 import com.jcasben.jchat.databinding.FragmentChatBinding
 
@@ -17,6 +18,7 @@ class ChatFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentChatBinding.inflate(layoutInflater, container, false)
+        binding.ivBack.setOnClickListener { findNavController().navigate(R.id.action_back) }
         return binding.root
     }
 }
--- JChat/app/src/main/java/com/jcasben/jchat/ui/home/HomeFragment.kt
+++ JChat/app/src/main/java/com/jcasben/jchat/ui/home/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.jcasben.jchat.R
 import com.jcasben.jchat.databinding.FragmentHomeBinding
 
@@ -13,10 +14,14 @@ class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+        binding.btnChat.setOnClickListener {
+            if (!binding.tietName.text.isNullOrEmpty()) {
+                findNavController().navigate(R.id.action_navigate_from_home_to_chat)
+            }
+        }
         return binding.root
     }
 }
