29b56843081447501a00374e1100f31d079f9667
jcasben <jcasben02@gmail.com>
2024-09-09 18:43:15 +0200

    [FEAT]: added navigation

--- JChat/app/build.gradle.kts
+++ JChat/app/build.gradle.kts
@@ -55,7 +55,7 @@ dependencies {
 
     // Navigation
     implementation(libs.androidx.navigation.fragment.ktx)
-    implementation(libs.androidx.navigation.fragment.ui.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
