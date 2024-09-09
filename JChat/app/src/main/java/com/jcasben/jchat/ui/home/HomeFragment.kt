e083c13559d4a2afa6148012d21e90f5ebd189c5
jcasben <jcasben02@gmail.com>
2024-09-09 16:32:44 +0200

    [NEW]: created JChat project

---
+++ JChat/app/src/main/java/com/jcasben/jchat/ui/home/HomeFragment.kt
@@ -0,0 +1,22 @@
+package com.jcasben.jchat.ui.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.jcasben.jchat.R
+import com.jcasben.jchat.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+}
